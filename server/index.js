0d0299c14112091dd4554ce746d796872856c755
justinfei9 <jpf99@yahoo.com>
2024-12-05 12:47:17 -0500

    added cors

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const WorkoutController = require("./controllers/workouts");
 const port = 3000;
 
 // Middleware
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.use(express.json());
 app.use(express.static(__dirname + "/dist"));
 
